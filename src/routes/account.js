11a98668c06bc390761d78ac4d5b22df82068e2e
Nathan Timothy <03nart@gmail.com>
2026-09-03 10:00:37 +0100

    fix: allow authenticated account requests from frontend

--- src/routes/account.js
+++ src/routes/account.js
@@ -5,9 +5,9 @@ const { getAccessState } = require("../services/access");
 function json(res, status, data) {
   res.writeHead(status, {
     "Content-Type": "application/json; charset=utf-8",
-    "Access-Control-Allow-Origin": process.env.FRONTEND_URL || "*",
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers":
-      "Content-Type, Authorization, X-API-Key, X-KitSetups-Device",
+      "Content-Type, Authorization, X-API-Key, X-KitSetups-Device, X-KitSetups-Fingerprint",
     "Access-Control-Allow-Methods":
       "GET,POST,PUT,PATCH,DELETE,OPTIONS",
   });
